6f0da0443fe9e918cc83899be7cc1f8213c74501
Mauri Gonçalves <maurigvs@icloud.com>
2022-09-19 16:21:58 -0300

    class 17

---
+++ src/main/kotlin/br/edu/mauri/Comments.kt
@@ -0,0 +1,16 @@
+package br.edu.mauri
+
+fun main () {
+
+    // Linha comentada
+
+    /*
+    Bloco comentado
+     */
+
+    //    fun test(){
+    //        /*
+    //        bloco comentado dentro do método
+    //         */
+    //    }
+}
